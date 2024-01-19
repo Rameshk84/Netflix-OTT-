7612f2789e21d96ca3daa4b9b805e170a8b60c83
Ramesh K <96737650+Rameshk84@users.noreply.github.com>
2024-01-19 08:15:54 +0530

    Files are Uploaded

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <img
+        className="nav__logo"
+        src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=700&h=456"
+        alt="Netflix-logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix-logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDw008hFBqe2mb_IgiYV5kHnoFecMjPBMs",
+    authDomain: "netflic-clone-dd49b.firebaseapp.com",
+    projectId: "netflic-clone-dd49b",
+    storageBucket: "netflic-clone-dd49b.appspot.com",
+    messagingSenderId: "304756066305",
+    appId: "1:304756066305:web:429e042129c8e69ee65800",
+    measurementId: "G-1YLED69KM9"
+  };
